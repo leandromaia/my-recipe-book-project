e4e9f7ebea823622476d8656c339e13c6f457968
zmailea <leandroc@inatel.br>
2019-03-24 18:23:57 -0300

    Change the recipe components to use a new service responsible to connect and serve the recipe data

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,10 @@
+import { EventEmitter } from '@angular/core';
+
 import { Recipe } from './recipe.model';
 
 export class RecipeService {
+    recipeSelected = new EventEmitter<Recipe>();
+
     recipes: Recipe[] = [
         new Recipe('Bolo de Cenoura', 'This is simply a test', 'https://melepimenta.com/wp-content/uploads/2018/01/Bolo-de-cenoura-de-liquidificador-Baixa-2-1024x683.jpg'),
         new Recipe('Bolo de Mandioca', 'This is simply a test', 'https://t2.rg.ltmcdn.com/pt/images/1/2/0/bolo_de_mandioca_cremoso_6021_paso_6_600.jpg')
